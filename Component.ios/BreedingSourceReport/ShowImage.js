b0133948130888a6505988a461406e1f388c76a4
abz53378 <a53378@yahoo.com.tw>
2016-05-11 23:06:29 +0800

    showimage button image

--- Component.ios/BreedingSourceReport/ShowImage.js
+++ Component.ios/BreedingSourceReport/ShowImage.js
@@ -15,19 +15,14 @@ import React, {
 import CONSTANTS from '../Global.js';
 import StatusBar from '../StatusBar.js';
 
-
-var BUTTONS = ['積水', '空屋'];
-var DESTRUCTIVE_INDEX = 3;
-var CANCEL_INDEX = 4;
 export default class ShowImage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            type:'住家',
+            type:'住家容器',
             description:'',
             lat:'',
             lon:'',
-            unmount: false,
         };
         //this.showActionSheet = this.showActionSheet.bind(this);
     };
@@ -45,75 +40,70 @@ export default class ShowImage extends Component {
         );
     };
     render() {
-        let types = ['住家','戶外a','戶外b'],
-        typeStyle = [styles.type, styles.type, styles.type];
-        for( let a in types){
-            if(types[a] === this.state.type){
-                typeStyle[a] = styles.typeSelected;
+        const types = ['住家容器','戶外容器','戶外髒亂處'];
+        let {type} = this.state,
+        imgObj = ['../../img/home', '../../img/outContainer', '../../img/outMess'];
+        for(let a in types){
+            switch (a) {
+                case '0':
+                let imgObj0 = types[0] === type ? require('../../img/homeSelected.png'):require('../../img/home.png');
                 break;
+                case '1':
+                let imgObj1 = types[1] === type ? require('../../img/outContainerSelected.png'):require('../../img/outContainer.png');
+                break;
+                case '2':
+                let imgObj2 = types[2] === type ? require('../../img/outMessSelected.png'):require('../../img/outMess.png');
+                break;
+                default:
             }
         }
-        if(!this.state.unmount){
-            return (
-                <ScrollView  ref="mount">
-                    <Image ref={'img'}style={styles.image} source={{uri: this.props.uri}}>
+        return (
+            <ScrollView  ref="mount">
+                <Image ref={'img'} style={styles.image} source={{uri: this.props.uri}}/>
+                <View style={styles.inputs}>
+                    <View style={styles.question}>
+                        <View style={styles.title}>
+                            <Text>
+                                孳生源類型
+                            </Text>
+                        </View>
+                        <View style={styles.types}>
+                            <TouchableHighlight  style={styles.type} underlayColor="#fff" onPress={()=>{this.setState({type:'住家容器'});}}>
+                                <Image style={styles.typeImage} source = {imgObj0}/>
 
-                    </Image>
-                    <View style={styles.inputs}>
-                        <View style={styles.question}>
-                            <View style={styles.title}>
-                                <Text>
-                                    孳生源類型
-                                </Text>
-                            </View>
-                            <View style={styles.types}>
-                                <TouchableHighlight  style={typeStyle[0]} underlayColor="#eee" onPress={()=>{this.setState({type:'住家'})}}>
-                                    <Text
-                                        style = {styles.typeText}
-                                        >
-                                        住家容器
-                                    </Text>
-                                </TouchableHighlight>
-                                <TouchableHighlight  style={typeStyle[1]} underlayColor="#eee" onPress={()=>{this.setState({type:'戶外a'})}}>
-                                    <Text
-                                        style = {styles.typeText}
-                                        >
-                                        戶外容器
-                                    </Text>
-                                </TouchableHighlight>
-                                <TouchableHighlight  style={typeStyle[2]} underlayColor="#eee" onPress={()=>{this.setState({type:'戶外b'})}}>
-                                    <Text
-                                        style = {styles.typeText}
-                                        >
-                                        戶外髒亂處
-                                    </Text>
-                                </TouchableHighlight>
-                            </View>
+                            </TouchableHighlight>
+                            <TouchableHighlight  style={styles.type} underlayColor="#fff" onPress={()=>{this.setState({type:'戶外容器'});}}>
+
+                                <Image style={styles.typeImage} source = {imgObj1}/>
+                            </TouchableHighlight>
+                            <TouchableHighlight  style={styles.type} underlayColor="#fff" onPress={()=>{this.setState({type:'戶外髒亂處'});}}>
+                                <Image style={styles.typeImage} source = {imgObj2}/>
+
+                            </TouchableHighlight>
                         </View>
-                        <View style = {styles.question}>
-                            <View style={styles.title}>
-                                <Text>
-                                    地點簡介
-                                </Text>
-                            </View>
-                            <View style={styles.answer}>
-                                <TextInput
-                                    multiline = {true}
-                                    style = {styles.textInput}
-                                    onChangeText = {(text) => this.setState({description:text})}
-                                    placeholder = "  description(大樹旁)"
-                                    >
-                                </TextInput>
-                            </View>
-                        </View>
-                        <TouchableHighlight style={styles.button} underlayColor="#eee" onPress={this.send.bind(this)}>
-                            <Text style={styles.buttonText}>送出</Text>
-                        </TouchableHighlight>
                     </View>
-                </ScrollView>
-            )
-        }
-        return null;
+                    <View style = {styles.question}>
+                        <View style={styles.title}>
+                            <Text>
+                                地點簡介
+                            </Text>
+                        </View>
+                        <View style={styles.answer}>
+                            <TextInput
+                                multiline = {true}
+                                style = {styles.textInput}
+                                onChangeText = {(text) => this.setState({description:text})}
+                                placeholder = "  description(大樹旁)"
+                                >
+                            </TextInput>
+                        </View>
+                    </View>
+                    <TouchableHighlight style={styles.button} underlayColor="#eee" onPress={this.send.bind(this)}>
+                        <Text style={styles.buttonText}>送出</Text>
+                    </TouchableHighlight>
+                </View>
+            </ScrollView>
+        )
     };
     send() {
 
@@ -148,62 +138,7 @@ export default class ShowImage extends Component {
         .catch(err => {
             console.warn(err);
         })
-        /*let obj = {
-        uri:this.props.uri, // either an 'assets-library' url (for files from photo library) or an image dataURL
-        uploadUrl:"http://140.116.247.113:11401/breeding_source/insert/",
-        //uploadUrl:"http://localhost:1337/breeding_source_report/",
-        fileName:fileName,
-        fileKey:'photo', // (default="file") the name of the field in the POST form data under which to store the file
-        mimeType:"multipart/form-data",
-        headers:{
-        Accept: "multipart/form-data",
-        "Content-Type":"multipart/form-data",
-        },
-        data: {
-        database:'tainan',
-        lat:lat,
-        lng:lon,
-        source_type: type,
-        description: description,
-        status: "未處理"
-        // whatever properties you wish to send in the request
-        // along with the uploaded file
-        }
-        };
-        if(lat === '' || lat === ''){
-        AlertIOS.alert("未開啟定位服務");
-        }
-        else if (description === '') {
-        AlertIOS.alert('請填寫資料');
-        }
-        else{
-        let subscription = NativeAppEventEmitter.addListener(
-        'transferring',
-        (response) => {
-        console.log("Transferring: percentage: ", response.progress * 100);
-        }
-        );
-        NativeModules.FileTransfer.upload(obj, (err, res) => {
-        console.log(res.status);
-        if(res.status === 200){
-        AlertIOS.alert(
-        '舉報成功'
-        );
-        this.props.toTop();
-        }
-        else{
-        AlertIOS.alert(
-        '舉報失敗'
-        );
-        console.warn(err);
-        }
-        subscription.remove();
-        // handle response
-        // it is an object with 'status' and 'data' properties
-        // if the file path protocol is not supported the status will be 0
-        // and the request won't be made at all
-        });
-        }*/
+
 
     }
 
@@ -250,31 +185,14 @@ var styles = StyleSheet.create({
         alignSelf:'center',
     },
 
-    type: {
-        height:50,
-        width:50,
-        alignItems:'center',
-        justifyContent: 'center',
-        marginHorizontal:10,
-        backgroundColor:'#fff',
-        borderRadius:25,
-        borderColor:"#777",
-        borderWidth:1,
+
+    typeImage: {
+        width: 50,
+        height: 50,
+        resizeMode: 'contain',
+        marginHorizontal:20,
     },
-    typeText: {
-        fontSize:10,
-    },
-    typeSelected: {
-        height:50,
-        width:50,
-        alignItems:'center',
-        justifyContent: 'center',
-        marginHorizontal:10,
-        backgroundColor:CONSTANTS.mainColor,
-        borderRadius:25,
-        borderColor:CONSTANTS.mainColor,
-        borderWidth:1,
-    },
+
     button: {
         backgroundColor: "#fff",
         width:70,
