9103e82c277514e87afae9be80d824d6bfd7875b
LeeW <cl87654321@gmail.com>
2019-01-28 11:20:20 +0800

    Add shell script to fix legacy code

---
+++ fix-module.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -f patch/RCTScrollView.m node_modules/react-native/React/Views/RCTScrollView.m
+cp -f patch/RCTSRWebSocket.m node_modules/react-native/Libraries/WebSocket/RCTSRWebSocket.m
